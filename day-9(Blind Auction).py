f5d56afdf51e761b97e6d1512b580af0dc7c6089
Muhammad Ali <133563431+MuhAliejaz47@users.noreply.github.com>
2023-07-01 12:33:25 +0500

    Add files via upload

---
+++ day-9(Blind Auction).py
@@ -0,0 +1,55 @@
+import os
+
+
+final_bid_list = []
+
+
+name = input('What is your name? ')
+bid = int(input('What are you willing to bid? '))
+
+
+bidding_dictionary = {
+    'name': name,
+    'bid': bid,
+}
+
+final_bid_list.append(bidding_dictionary)
+
+direction = input('Are there any more bidders? Type "n" for no and "y" for yes: ')
+
+
+def function(name, bid, direction):
+    name = input('What is your name? ')
+    bid = int(input('What are you willing to bid? '))
+
+    bidding_dictionary = {
+        'name': name,
+        'bid': bid,
+    }
+    final_bid_list.append(bidding_dictionary)
+
+    direction = input('Are there any more bidders? Type "n" for no and "y" for yes: ')
+    if direction == 'y':
+        function(name, bid, direction)
+    if direction == 'n':
+        determine_winner()
+
+
+def determine_winner():
+    if len(final_bid_list) == 1:
+        winner = final_bid_list[0]
+    else:
+        highest_bid = 0
+        winner = None
+        for bidder in final_bid_list:
+            if bidder['bid'] > highest_bid:
+                highest_bid = bidder['bid']
+                winner = bidder
+
+    print(f'The winner is {winner["name"]} with a bid of {winner["bid"]}')
+
+
+if direction == 'y':
+    function(name, bid, direction)
+if direction == 'n':
+    determine_winner()
